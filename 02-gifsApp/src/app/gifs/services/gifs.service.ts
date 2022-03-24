25fc8364cf6a59fe27430e2ed130b042199dc416
Jaime-Ryzen5 <kovenam@gmail.com>
2022-03-24 23:46:23 +0100

    92.

--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,31 +8,45 @@ import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 export class GifsService {
 
   private _apiKey: string = 'GnSTEUFRIXNIp0xtekN1y9aMLdG5ighE';
+  private servicioURL: string = 'https://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
 
-  public resultados: Gif[] =[];
+  public resultados: Gif[] = [];
 
   get historial() {
     return [...this._historial]; // [...] Rompe la referencia, no sé que significa
   }
 
   constructor(private http: HttpClient) {
-
+    // if (localStorage.getItem('historial')) {
+    //   this._historial = JSON.parse(localStorage.getItem('historial')! ); // Con ! le dices a angular confía en mi, se que este objeto es válido
+    // }
+    
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || []; // || []--> Si es null devuelve un array vacio
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
   }
 
-  buscarGifs(query: string) {
+  buscarGifs(query: string = '') {
 
     query = query.trim().toLocaleLowerCase();
 
     if (!this._historial.includes(query)) { // insertar solo si no ya existe en la lista
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10); // mostrar solo los 10 últimos
+      
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+      
     }
 
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=GnSTEUFRIXNIp0xtekN1y9aMLdG5ighE&q=${query}&limit=10`)
+    const params = new HttpParams()
+      .set('api_key', this._apiKey)      
+      .set('limit', '10')
+      .set('q', query);
+
+    this.http.get<SearchGifsResponse>(`${this.servicioURL}/search`, {params})
       .subscribe((resp) => {
-        console.log(resp.data);
         this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
       })
 
   }
