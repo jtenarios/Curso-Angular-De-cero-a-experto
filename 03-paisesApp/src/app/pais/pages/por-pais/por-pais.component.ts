1a91394cb10f16a6eabbed0f901a24ea075eefa1
Jaime-Ryzen5 <kovenam@gmail.com>
2022-04-19 00:36:30 +0200

    113. Por Capital

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Country } from '../../interfaces/paises.interface';
 import { PaisService } from '../../services/pais.service';
 
