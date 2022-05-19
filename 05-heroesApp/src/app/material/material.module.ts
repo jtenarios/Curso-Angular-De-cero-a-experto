b4e3ab467de2267c4cc794b9e6fea6682055afc9
Jaime-Ryzen5 <kovenam@gmail.com>
2022-05-19 23:59:44 +0200

    208. Insertar en base de datos

--- 05-heroesApp/src/app/material/material.module.ts
+++ 05-heroesApp/src/app/material/material.module.ts
@@ -10,6 +10,7 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatSelectModule} from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -27,6 +28,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatFormFieldModule,
     MatListModule,
     MatProgressSpinnerModule,
+    MatSelectModule,
     MatSidenavModule,
     MatToolbarModule
   ]
