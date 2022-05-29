04dc8236fb5ce6fa26ec3fbf00fe3d22069668b4
Jaime-Ryzen5 <kovenam@gmail.com>
2022-05-30 00:11:20 +0200

    241. Template: Formularios dinámicos y arreglos

--- 06-formulariosApp/src/app/template/basicos/basicos.component.ts
+++ 06-formulariosApp/src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -9,13 +9,39 @@ import { NgForm } from '@angular/forms';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
+  initForm = {
+    producto: 'RTX 4080ti',
+    precio: 10,
+    existencias: 10
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  guardar( miFormulario : NgForm ) {
-    console.log('miFormulario', miFormulario.value);
+  nombreValido(): boolean {
+    return this.miFormulario?.controls['producto']?.invalid &&
+      this.miFormulario?.controls['producto']?.touched;
   }
+
+  precioValido(): boolean {
+    return this.miFormulario?.controls['precio']?.value < 0 &&
+      this.miFormulario?.controls['precio']?.touched;
+  }
+
+  guardar() {
+    console.warn('Posteo correcto');
+
+    this.miFormulario.resetForm({
+      producto: 'Sin nombre',
+      precio: 0,
+      existencias: 0
+    });
+  }
+
+
 
 }
---
+++ 06-formulariosApp/src/app/template/directives/custom-min.directives.ts
@@ -0,0 +1,32 @@
+import { Directive, Input } from "@angular/core";
+import { FormControl, NG_VALIDATORS, Validators } from "@angular/forms";
+
+@Directive({
+    selector: '[customMin][ngModel]',
+    providers: [{
+        provide: NG_VALIDATORS,
+        useExisting: CustomMinDirective,
+        multi: true
+    }]
+})
+
+
+export class CustomMinDirective implements Validators {
+
+    @Input() minimo!: number;
+
+    constructor() {
+        console.log('Directiva', this.minimo);
+    }
+
+    validate(control: FormControl) {
+        const inputValue = control.value;
+
+        console.log('validate', inputValue, 'minimo', this.minimo);
+
+        return (inputValue < this.minimo)
+            ? { 'customMin': true }
+            : null;
+    }
+
+}
