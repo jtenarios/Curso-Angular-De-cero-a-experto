037be9a8f1da51a368cea5518fc015c0ee4a709f
Jaime-Ryzen5 <kovenam@gmail.com>
2022-06-09 23:42:33 +0200

    crear rutas y lazy load

--- 07-selectores/src/app/app-routing.module.ts
+++ 07-selectores/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SelectorPageComponent } from './paises/pages/selector-page/selector-page.component';
+import { PaisesModule } from './paises/paises.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'selector',
+    loadChildren: () => import('./paises/paises.module').then(m => m.PaisesModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'selector'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
