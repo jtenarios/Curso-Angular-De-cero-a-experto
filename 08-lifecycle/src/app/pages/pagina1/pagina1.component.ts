968e64c9fb8c51f08d191eef27191917c9cffe29
Jaime-Ryzen5 <kovenam@gmail.com>
2022-06-12 17:22:55 +0200

    Inicio proyecto Mapas

---
+++ 08-lifecycle/src/app/pages/pagina1/pagina1.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-pagina1',
+  templateUrl: './pagina1.component.html',
+  styles: [
+  ]
+})
+export class Pagina1Component implements OnInit, OnChanges, DoCheck,
+  AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked, OnDestroy {
+
+  nombre: string = 'Jaime';
+  segundo: number = 0;
+  timerSubscription!: Subscription;
+
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.timerSubscription = interval(1000).subscribe( i => {
+      this.segundo = i;
+    })
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy.');
+    this.timerSubscription.unsubscribe();
+    console.log('Timer limpiado');
+  }
+
+  guardar() {
+
+  }
+
+
+
+}
