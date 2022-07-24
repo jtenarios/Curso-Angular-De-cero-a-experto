66a3f237e3f8a9ed8b85cc6eca984947e8ce6095
Jaime-Ryzen5 <kovenam@gmail.com>
2022-07-24 21:03:53 +0200

    390. Login de usuario desde Angular

---
+++ 12-authApp/src/app/auth/interfaces/interfaces.ts
@@ -0,0 +1,9 @@
+export interface AuthResponse {    
+        ok: boolean;
+        uid?: string;
+        name?: string;
+        token?: string;
+        msg?: string;
+
+    
+}
--- 12-authApp/src/app/auth/pages/login/login.component.ts
+++ 12-authApp/src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -16,12 +17,18 @@ export class LoginComponent {
   })
 
   constructor(private fb: FormBuilder,
-    private router: Router) { }
+    private router: Router,
+    private authService: AuthService) { }
 
   login() {
     console.log(this.miFormulario.value);
+    const {email, password} = this.miFormulario.value;
 
-    this.router.navigateByUrl('/dashboard');
+    this.authService.login(email, password)
+    .subscribe( resp => {
+      console.log(resp);
+    });
+    // this.router.navigateByUrl('/dashboard');
   }
 
 
