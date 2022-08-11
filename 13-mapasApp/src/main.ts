07e5c03f7a04d631d4f297d86869679607b24906
Jaime Tena Rios <jaime.tena@onyxcentersource.com>
2022-08-11 22:48:37 +0200

    412. Obtener la geolocalización del usuario

--- 13-mapasApp/src/main.ts
+++ 13-mapasApp/src/main.ts
@@ -4,6 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+if (!navigator.geolocation) {
+  alert('Navegador no soporta la geolocation');
+  throw new Error('Navegador no soporta la geolocation');
+}
+
 if (environment.production) {
   enableProdMode();
 }
